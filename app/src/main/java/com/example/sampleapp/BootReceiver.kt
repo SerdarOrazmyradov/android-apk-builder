1f48485758f530a12fd85f14829867dd276bd17e
SerdarOrazmyradov <mathematicaismylife@gmail.com>
2026-09-02 18:46:37 +0500

    add :to code QUICKBOOT_POWERON

--- app/src/main/java/com/example/sampleapp/BootReceiver.kt
+++ app/src/main/java/com/example/sampleapp/BootReceiver.kt
@@ -6,7 +6,11 @@ import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        val action = intent.action
+        if (action == Intent.ACTION_BOOT_COMPLETED ||
+            action == "android.intent.action.QUICKBOOT_POWERON" ||
+            action == "com.htc.intent.action.QUICKBOOT_POWERON") {
+
             val serviceIntent = Intent(context, GatewayService::class.java)
             context.startService(serviceIntent)
         }
